93dcdf755356f4d4658de226c39070faadb1edc9
Rihana Ries <rihana.ries@noice.work>
2021-06-15 22:32:21 +1000

    Volume ramping function

--- js/instruments.js
+++ js/instruments.js
@@ -1,21 +1,58 @@
+// Volume controller
+class VolumeController {
+    constructor(volumeValue, tick, instrument) {
+        this.volumeValue = volumeValue; // The volume in decibels
+        this.tick = tick; // The frequency of ramping in milliseconds
+        this.instrument = instrument; // The instrument that the controller affects
+    }
+}
+
+async function rampVolume(VolumeController) {
+    if (VolumeController.volumeValue) {
+        // Set the initial volume value of the instrument
+        VolumeController.instrument.volume.value = VolumeController.volumeValue[0];
+        for (let i = 0; i <= VolumeController.volumeValue.length; i++) {
+            if (playing) {
+                // Ramp the volume up or down according to the volume data, clamped to protect your speakers from rogue data
+                VolumeController.instrument.volume.value = Math.min(Math.max(parseInt(VolumeController.volumeValue[i]), -20), 0);
+                await sleep(VolumeController.tick);
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+}
+
+
+// Effects rack
+
+const distortion = new Tone.Distortion(0.8).toDestination();
+const reverb = new Tone.Reverb(4).toDestination();
+
+
+// Instruments
+
 const synth = new Tone.Synth({
     oscillator: {
         type: "sine"
     }
 });
-const reverb = new Tone.Reverb(4).toDestination();
+
 synth.connect(reverb);
 
-const distortion = new Tone.Distortion(0.8).toDestination();
+
 const coal = new Tone.MonoSynth({
     oscillator: {
         type: "sine"
     },
     envelope: {
-        attack: 0.2,
-        decay: 0.01,
-        sustain: 0.01,
-        release: 0.01,
+        attack: 0.07,
+        decay: 0.08,
+        sustain: 0.085,
+        release: 0.095,
     }
-});
-coal.connect(distortion);
+}).toDestination();
+
+// coal.connect(distortion);
--- js/metronome.js
+++ js/metronome.js
@@ -5,9 +5,13 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 async function rampTempo(tempos) {
     if (tempos) {
         for (let i = 0; i <= tempos.length; i++) {
-            await sleep(1000);
-            console.log(tempos[i]);
-            Tone.Transport.bpm.rampTo(tempos[i], 1);
+            if (playing) {
+                await sleep(1000);
+                console.log(tempos[i]);
+                Tone.Transport.bpm.rampTo(tempos[i], 1);
+            } else {
+                return false;
+            }
         }
     } else {
         return false;
