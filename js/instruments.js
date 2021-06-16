135cd8c7dea8cef7716ab5340f9ae9d854e87b77
Rihana Ries <rihana.ries@noice.work>
2021-06-17 02:51:18 +1000

    UI, gas synth

--- js/instruments.js
+++ js/instruments.js
@@ -26,24 +26,22 @@ async function rampVolume(VolumeController) {
 }
 
 
-// Effects rack
-
-const distortion = new Tone.Distortion(0.8).toDestination();
-const reverb = new Tone.Reverb(4).toDestination();
-
-
 // Instruments
+let synth;
+let coal;
+let gas;
+let hydro;
+let solar;
+let wind;
 
-const synth = new Tone.Synth({
+
+synth = new Tone.Synth({
     oscillator: {
         type: "sine"
     }
 });
 
-synth.connect(reverb);
-
-
-const coal = new Tone.MonoSynth({
+coal = new Tone.MonoSynth({
     oscillator: {
         type: "sine"
     },
@@ -55,4 +53,50 @@ const coal = new Tone.MonoSynth({
     }
 }).toDestination();
 
+gas = new Tone.AMSynth({
+    harmonicity: 2,
+    oscillator: {
+        type: "sine",
+    },
+    envelope: {
+        attack: 0.04,
+        decay: 0.4,
+        sustain: 0.5,
+        release: 0.2,
+    }
+}).toDestination();
+
+hydro = new Tone.Synth({
+    oscillator: {
+        type: "sine"
+    },
+}).toDestination();
+
+solar = new Tone.Synth({
+    oscillator: {
+        type: "sine"
+    },
+}).toDestination();
+
+wind = new Tone.Synth({
+    oscillator: {
+        type: "sine"
+    },
+}).toDestination();
+
+// Effects rack
+
+const distortion = new Tone.Distortion(0.8).toDestination();
+const reverb = new Tone.Reverb(4).toDestination();
+const compressor = new Tone.Compressor(-30,1);
+const chorus = new Tone.Chorus(1, 1, 1).toDestination().start();
+const feedbackDelay = new Tone.FeedbackDelay("8n", 0.5).toDestination();
+const tremolo = new Tone.Tremolo(3, 0.75).toDestination().start();
+
+
+synth.connect(reverb);
+gas.connect(chorus);
+gas.connect(feedbackDelay);
+gas.connect(tremolo);
+compressor.toDestination();
 // coal.connect(distortion);
